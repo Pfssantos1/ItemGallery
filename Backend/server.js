048e07ee71840bb156185f394dbd4b2cf2a234f1
Pfssantos1 <pedro_felipe_02@hotmail.com>
2025-06-27 01:07:48 -0300

    Delete Backend directory

--- Backend/server.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('🟢 MongoDB conectado com sucesso'))
-  .catch(err => console.error('🔴 Erro ao conectar ao MongoDB:', err));
